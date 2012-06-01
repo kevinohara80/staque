050829410619138b65345472647d393ce1299a0d
Kevin O'Hara <kevinohara80@gmail.com>
2012-06-01 11:01:46 -0400

    removing a test file

--- tests/test.js
+++
@@ -1,7 +0,0 @@
-var staque = require('../');
-var test = require("tap").test
-
-test('check the thing', function(t){
-  t.equal(staque.version !== null, true, 'check for existence of version');
-  t.end();
-});
