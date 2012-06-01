6bc356efc06dade81f52607cbc47136f537f9fe2
Kevin O'Hara <kevinohara80@gmail.com>
2012-06-01 08:54:08 -0400

    first real test

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 var async = require('async');
 
+var Staque = function(opts) {
+  
+}
 
+module.exports.createStaque = function(opts) {
+  return new Staque(opts);
+}
 
 module.exports.version = '0.0.1';
--- tests/test.js
+++ tests/test.js
@@ -1,6 +1,7 @@
+var staque = require('../');
 var test = require("tap").test
 
 test('check the thing', function(t){
-  t.equal(true,true);
+  t.equal(staque.version !== null, true, 'check for existence of version');
   t.end();
 });
